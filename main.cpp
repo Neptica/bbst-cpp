0030568fe1cacc100bfca5023241c35f9fa51737
Michael Mumme <michaelmumme@me.com>
2025-02-01 22:00:31 -0600

    add delete method to tree

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -8,9 +9,40 @@ struct Node {
   Node *left;
   Node *right;
 
+  bool operator==(const Node *other) const {
+    return val == other->val && parent == other->parent &&
+           left == other->left && right == other->right;
+  }
+
   Node(int value, Node *daddy) {
     parent = daddy;
     val = value;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Node *node) {
+    std::stringstream entity;
+    entity << "\n\nValue: " << node->val;
+    entity << "\nParent: ";
+    if (node->parent) {
+      entity << node->parent->val;
+    } else {
+      entity << "nullptr ";
+    }
+
+    entity << "\nleft: ";
+    if (node->left) {
+      entity << node->left;
+    } else {
+      entity << "nullptr ";
+    }
+
+    entity << "\nRight: ";
+    if (node->right) {
+      entity << node->right;
+    } else {
+      entity << "nullptr ";
+    }
+    return os << entity.str();
   }
 };
 
@@ -19,19 +51,6 @@ private:
   Node *root;
 
 public:
-  // const prettyPrint = (node, prefix = "", isLeft = true) => {
-  //   if (node === null) {
-  //     return;
-  //   }
-  //   if (node.right !== null) {
-  //     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  //   }
-  //   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  //   if (node.left !== null) {
-  //     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  //   }
-  // };
-
   void prettyPrint(Node *node, std::string prefix = "", bool isLeft = true) {
     if (node) {
       if (node->right) {
@@ -86,13 +105,85 @@ public:
     }
     return root;
   }
+
+  Node *deleteNode(int value) {
+    Node *deletedNode = find(value);
+    if (!deletedNode) {
+      return nullptr;
+    }
+
+    Node *successor;
+    if (deletedNode->right) {
+      if (deletedNode->right->left) {
+        successor = deletedNode->right->left;
+        while (successor->left) {
+          successor = successor->left;
+        }
+
+        if (deletedNode->parent) {
+          successor->parent->left = successor->right;
+        }
+        successor->right = deletedNode->right;
+      } else {
+        successor = deletedNode->right;
+      }
+      successor->parent = deletedNode->parent;
+      successor->left = deletedNode->left;
+      if (deletedNode->left) {
+        deletedNode->left->parent = successor;
+      }
+
+      if (deletedNode->parent) {
+        successor->parent->right = successor;
+      } else {
+        root = successor;
+      }
+
+    } else {
+      successor = deletedNode->left;
+      if (deletedNode->parent) {
+        if (deletedNode->parent->left == deletedNode) {
+          deletedNode->parent->left = successor;
+        } else {
+          deletedNode->parent->right = successor;
+        }
+        if (deletedNode->left) {
+          successor->parent = deletedNode->parent;
+        }
+      } else {
+        root = successor;
+      }
+    }
+    delete deletedNode;
+
+    // rebalance(successor);
+    return root;
+  }
+
+  Node *find(int value) {
+    Node *curr = root;
+    while (curr) {
+      if (value < curr->val) {
+        curr = curr->left;
+      } else if (value > curr->val) {
+        curr = curr->right;
+      } else {
+        return curr;
+      }
+    }
+    return nullptr;
+  }
 };
 
 int main() {
   Tree *tree = new Tree();
 
-  std::vector<int> values = {43, 57, 87, 97, 27, 4};
+  std::vector<int> values = {43, 57, 87, 97, 27, 4, 70, 90, 91};
   Node *root = tree->buildTree(values);
 
+  tree->prettyPrint(root);
+  root = tree->deleteNode(43);
+  root = tree->deleteNode(87);
+  // std::cout << root << "\n";
   tree->prettyPrint(root);
 }
